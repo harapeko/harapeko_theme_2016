8bcf25a99e5520982fbc0f355f1c0c5473099fdf
harapeko <tea.lile.tea@gmail.com>
2016-04-25 15:08:12 +0000

    fix footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="l_footer" role="contentinfo">
   <div class="l_footer_innter l_footer_site_info">
-  	<small>Copyright2013</small>
+  	<small>Copyright2011</small>
   	<strong><a class="l_footer_site_info_name" href="/" role="home">はらぺこ屋</a></strong>
   	<a class="icon" href="https://twitter.com/harapeko_wktk">
       <i class="fa fa-twitter-square"></i>
