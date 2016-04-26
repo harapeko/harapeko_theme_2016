a6a463104d1350cd08c8b9995cdc968400e53a05
harapeko <tea.lile.tea@gmail.com>
2016-04-26 06:07:18 +0000

    for json-ld org

--- footer.php
+++ footer.php
@@ -15,5 +15,23 @@
 <script type="text/javascript" src="/wp-content/themes/harapeko_theme_2016/js/jquery.scrolldepth.min.js"></script>
 <script type="text/javascript" src="/wp-content/themes/harapeko_theme_2016/js/app.js"></script>
 <?php wp_footer(); ?>
+
+<script type="application/ld+json">
+// Organization + Logo
+{
+  "@context" : "http://schema.org",
+  "@type" : "Organization",
+  "name" : "はらぺこ屋",
+  "alternateName" : "はらぺこ屋", // Include Site Name in Search Results
+  "url" : "http://harapeko.wktk.so",
+  "logo": "https://pbs.twimg.com/profile_images/683576596129689601/9e7apLrW.png",
+  "sameAs" : [
+    "https://www.facebook.com/harapeko.wktk.so",
+    "https://twitter.com/harapeko_wktk",
+    "https://plus.google.com/u/0/+%E3%81%AF%E3%82%89%E3%81%BA%E3%81%93%E3%81%AF%E3%82%89%E3%81%B8%E3%81%A3%E3%81%9F",
+    "https://www.youtube.com/channel/UCA_cwdt4lwon7PzBCj9Eqww"
+  ]
+}
+</script>
 </body>
 </html>
